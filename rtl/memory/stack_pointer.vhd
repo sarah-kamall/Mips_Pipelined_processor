5af87b1e351733b647ef8176b7a1825ad39ef434
sarah-kamall <sarah.zaky02@eng-st.cu.edu.eg>
2024-12-19 17:56:17 +0200

    feat: add stack pointer and its control unit

---
+++ rtl/memory/stack_pointer.vhd
@@ -0,0 +1,31 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.STD_LOGIC_ARITH.all;
+use IEEE.STD_LOGIC_UNSIGNED.all;
+
+entity stack_pointer is
+  port (
+    clk  : in std_logic;
+    rst  : in std_logic;
+    load : in std_logic;
+    address_in  : in std_logic_vector(15 downto 0);
+    dout : out std_logic_vector(15 downto 0)
+  );
+end entity;
+
+architecture Behavioral of stack_pointer is
+  signal reg_data : std_logic_vector(15 downto 0);
+begin
+  process (clk, rst)
+  begin
+    if rst = '1' then
+      reg_data <= (others => '0');
+    elsif rising_edge(clk) then
+      if load = '1' then
+        reg_data <= address_in;
+      end if;
+    end if;
+  end process;
+
+  dout <= reg_data;
+end architecture;
